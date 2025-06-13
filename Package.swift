d60bd94bb811c44a24c2c653ba12fba5b996217d
caiomello <github@caiomello.com>
2025-06-13 21:04:51 +0100

    Update tooling

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Persistence",
     platforms: [
-        .iOS("15.0"),
-        .macOS("12.0")
+        .iOS("26"),
+        .macOS("26")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
